d3498fbc98496a965216f72a981b8d5a9eafbb29
Ina-Nako <inanako19@gmail.com>
2024-07-16 14:27:52 +0200

    space fixing

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,13 +1,14 @@
+// 2-then.js
 export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => {
       console.log('Got a response from the API');
-      return {
-        status: 200,
-        body: 'success'
-        };
+      return { status: 200, body: 'success' };
     })
     .catch(() => {
       return new Error();
+    })
+    .finally(() => {
+      console.log('Got a response from the API');
     });
 }
