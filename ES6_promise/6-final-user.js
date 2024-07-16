036ac5ba2e9472f5c53c9295e40c42fbbc709167
Ina-Nako <inanako19@gmail.com>
2024-07-16 21:29:27 +0200

    space fixing

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,10 +1,24 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
-
+/*eslint-disable*/
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
+  const userPromise = signUpUser(firstName, lastName)
+    .then((value) => ({
+      status: "fulfilled",
+      value,
+    }))
+    .catch((error) => ({
+      status: "rejected",
+      value: error.toString(),
+    }));
+  const photoPromise = uploadPhoto(fileName)
+    .then((value) => ({
+      status: "fulfilled",
+      value,
+    }))
+    .catch((error) => ({
+      status: "rejected",
+      value: error.toString(),
+    }));
+  return Promise.all([userPromise, photoPromise]);
 }
