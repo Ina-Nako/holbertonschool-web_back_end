22093f3a2314f88b55e199c5fb413dc0cc76d6dc
Ina-Nako <inanako19@gmail.com>
2024-07-25 19:06:25 +0200

    ex 3

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,5 @@
+export default function getStudentIdsSum(listStudents) {
+  const initialValue = 0;
+  const sum = listStudents.reduce((accumulator, student) => accumulator + student.id, initialValue);
+  return sum;
+}
