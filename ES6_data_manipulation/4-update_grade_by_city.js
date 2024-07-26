840fcb4585b5010d4fcf7d16c46f53052d22b1cf
Ina-Nako <inanako19@gmail.com>
2024-07-26 17:56:27 +0200

    ex 4

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,13 @@
+export default function updateStudentGradeByCity(listOfStudent, city, newGrade) {
+  const filterStudent = listOfStudent.filter((student) => student.location === city);
+  const addGrade = filterStudent.map((student) => {
+    const grade = newGrade.find((grade) => grade.studentId === student.id);
+    if (grade) {
+      student.grade = grade.grade;
+    } else {
+      student.grade = 'N/A';
+    }
+    return student;
+  });
+  return addGrade;
+}
