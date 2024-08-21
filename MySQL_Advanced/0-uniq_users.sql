5c914e9a892ddfdafff73a75129a18e4de0734af
Ina-Nako <inanako19@gmail.com>
2024-08-21 18:06:04 +0200

    ex 0

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,8 @@
-
-CREATE TABLE IF NOT EXISTS users(
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, 
-    email VARCHAR(255) NOT NULL UNIQUE, 
-    name VARCHAR(255));
+-- Make a table
+-- If table exist, you should not fail
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY(id)
+);
