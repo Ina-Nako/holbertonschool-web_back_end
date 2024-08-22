d2ed9cc4d9af9fcb00bfaf544df5782b8840269b
Ina-Nako <inanako19@gmail.com>
2024-08-22 18:18:52 +0200

    ex 3

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
 -- Make a table
 -- If table exist, you should not fail
-SELECT band_name, IFNULL(split, 2023) - formed as lifespan
+SELECT band_name, IFNULL(split, 2024) - formed as lifespan
 FROM metal_bands WHERE style LIKE "%Glam rock%" ORDER BY lifespan DESC;
