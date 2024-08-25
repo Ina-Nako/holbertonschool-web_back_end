0f4410078a6d128c5e78189ef720b3ca234c70f2
Ina-Nako <inanako19@gmail.com>
2024-08-25 21:36:59 +0200

    ex 7

---
+++ MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,16 @@
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN p_user_id INT
+)
+BEGIN
+    DECLARE v_average_score FLOAT;
+
+    -- Calculate the average score for the given user
+    SELECT AVG(score) INTO v_average_score
+    FROM corrections
+    WHERE user_id = p_user_id;
+
+    -- Update the user's average score in the users table
+    UPDATE users
+    SET average_score = v_average_score
+    WHERE id = p_user_id;
+END;
