814002af3e862618734b22e1e29b8b3612d3e732
Ina-Nako <inanako19@gmail.com>
2024-08-25 20:23:59 +0200

    ex 6

--- MySQL_Advanced/6-bonus.sql
+++ MySQL_Advanced/6-bonus.sql
@@ -1,28 +1,29 @@
+-- Script: add_bonus_procedure.sql
+
 DELIMITER //
 
 CREATE PROCEDURE AddBonus(
-    IN p_user_id INT,
-    IN p_project_name VARCHAR(255),
-    IN p_score INT
+    IN user_id INT,
+    IN project_name VARCHAR(255),
+    IN score INT
 )
 BEGIN
-    DECLARE v_project_id INT;
-
     -- Check if the project already exists
-    SELECT id INTO v_project_id
-    FROM projects
-    WHERE name = p_project_name
-    LIMIT 1;
+    DECLARE project_id INT;
 
-    -- If the project doesn't exist, create it
-    IF v_project_id IS NULL THEN
-        INSERT INTO projects (name) VALUES (p_project_name);
-        SET v_project_id = LAST_INSERT_ID();
+    SELECT id INTO project_id
+    FROM projects
+    WHERE name = project_name;
+
+    -- If the project does not exist, insert it and get the new id
+    IF project_id IS NULL THEN
+        INSERT INTO projects (name) VALUES (project_name);
+        SET project_id = LAST_INSERT_ID();
     END IF;
 
-    -- Insert the correction
-    INSERT INTO corrections (user_id, project_id, score)
-    VALUES (p_user_id, v_project_id, p_score);
-END //
+    -- Insert the correction for the user and project
+    INSERT INTO corrections (user_id, project_id, score) 
+    VALUES (user_id, project_id, score);
+END//
 
 DELIMITER ;
