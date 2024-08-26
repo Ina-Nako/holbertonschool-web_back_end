f26fa5f531a39c8aa6eff97743f6c8ea34cacec3
Ina-Nako <inanako19@gmail.com>
2024-08-26 18:05:59 +0200

    ex 7

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,17 +1,16 @@
--- Create the stored procedure
+-- Set the delimiter to $$ to avoid issues with semicolons
+
 DELIMITER $$
 
-DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
-
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN input_user_id INT)
 BEGIN
-    UPDATE users
-    SET
-       average_score = (
-        SELECT AVG(score) FROM correction
-        WHERE correction.user_id = user_id
-      )
-    WHERE id = user_id
+    UPDATE users 
+    SET average_score = (
+        SELECT AVG(score)
+        FROM corrections
+        WHERE corrections.user_id = input_user_id
+    )
+    WHERE id = input_user_id;
 END $$
 
 DELIMITER ;
